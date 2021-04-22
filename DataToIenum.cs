386825f85a3fc19f6d59e195e1de5c0a8f28f2ad
ZvalsoraY <yazamail@yandex.ru>
2021-04-22 22:48:43 +0300

    starting

--- DataToIenum.cs
+++ DataToIenum.cs
@@ -1,21 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace Clope_mash
 {
     class DataToIenum
     {
-        public static IEnumerable<IEnumerable<double>> FileDoubleArrayList(string filePath)
+        public static IEnumerable<IEnumerable<string>> TransactionCharList(string filePath)
         {
             var lines = File.ReadLines(filePath);
-            //return lines.Select(line =>
-            //  line.Split(new[] { ' ', '!' }, StringSplitOptions.RemoveEmptyEntries).Select(s =>
-            //      double.Parse(s, NumberStyles.Any, CultureInfo.InvariantCulture))).OrderBy(u => u.ElementAt(1));
             return lines.Select(line =>
-              line.Split(new[] { ' ', '!' }, StringSplitOptions.RemoveEmptyEntries).Select(s =>
-              Math.Round(double.Parse(s, NumberStyles.Any, CultureInfo.InvariantCulture), 2, MidpointRounding.AwayFromZero)
-                  )).OrderBy(u => u.ElementAt(1));
+              line.Split(new[] { ' ', '!', '?', ','}, StringSplitOptions.RemoveEmptyEntries).Select(s => s));
+            
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,6 +6,12 @@ namespace Clope_mash
     {
         static void Main(string[] args)
         {
+            foreach (var rowData in DataToIenum.TransactionCharList("agaricus-lepiota.data"))
+            {
+                foreach (var s in rowData) Console.Write(s, " ");
+                Console.WriteLine();
+            }
+                
             Console.WriteLine("Hello World!");
 
             
