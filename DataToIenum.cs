f977ae9aadd041f8b03954cb8fc3f16ab0e9e440
ZvalsoraY <yazamail@yandex.ru>
2021-04-22 21:33:56 +0300

    Add project files.

---
+++ DataToIenum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clope_mash
+{
+    class DataToIenum
+    {
+        public static IEnumerable<IEnumerable<double>> FileDoubleArrayList(string filePath)
+        {
+            var lines = File.ReadLines(filePath);
+            //return lines.Select(line =>
+            //  line.Split(new[] { ' ', '!' }, StringSplitOptions.RemoveEmptyEntries).Select(s =>
+            //      double.Parse(s, NumberStyles.Any, CultureInfo.InvariantCulture))).OrderBy(u => u.ElementAt(1));
+            return lines.Select(line =>
+              line.Split(new[] { ' ', '!' }, StringSplitOptions.RemoveEmptyEntries).Select(s =>
+              Math.Round(double.Parse(s, NumberStyles.Any, CultureInfo.InvariantCulture), 2, MidpointRounding.AwayFromZero)
+                  )).OrderBy(u => u.ElementAt(1));
+        }
+    }
+}
